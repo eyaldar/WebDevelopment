c8ba204dc37978d16b76fe66a0c0beb61f5b74fe
eyaldar <eyaldar@gmail.com>
2014-11-30 02:08:13 +0200

    Changed the references to be relative instead of absolute.

---
+++ MusicRoom/src/com/musicroom/database/RedisManager.java
@@ -0,0 +1,16 @@
+package com.musicroom.database;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisManager {
+	
+	private static Jedis connection;
+	
+	public static Jedis getConnection() {
+		if(connection == null) {
+			connection = new Jedis("localhost");
+		}
+		
+		return connection;
+	}
+}
--- MusicRoom/src/com/musicroom/servlets/MusicRoomServlet.java
+++ MusicRoom/src/com/musicroom/servlets/MusicRoomServlet.java
@@ -1,12 +1,15 @@
 package com.musicroom.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.musicroom.database.RedisManager;
 
 /**
  * Servlet implementation class MusicRoomServlet
@@ -41,7 +44,6 @@ public class MusicRoomServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
