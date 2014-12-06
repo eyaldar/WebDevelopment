f2496ed287798c74142d1c75efc13c14e82cff77
user <user@user-PC>
2014-12-06 20:18:03 +0200

    set session manager

--- MusicRoom/src/com/musicroom/session/SessionManager.java
+++ MusicRoom/src/com/musicroom/session/SessionManager.java
@@ -1,18 +1,26 @@
 package com.musicroom.session;
 
 import javax.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
 
 public class SessionManager {
 
-	static final String USERNAME = "Username";
+	static final String USER = "user";
 
-	public static String getLoggedInUsername(HttpServletRequest request) {
-		Object sessionAttribute = request.getSession().getAttribute(USERNAME);
-		return sessionAttribute != null ? sessionAttribute.toString() : null;
+	public static JSONObject getLoggedInUser(HttpServletRequest request) 
+	{
+		Object sessionAttribute = request.getSession().getAttribute(USER);
+		return sessionAttribute != null ? (JSONObject)sessionAttribute : null;
 	}
 
-	public static void setLoggedInUsername(HttpServletRequest request,
-			String name) {
-		request.getSession().setAttribute(USERNAME, name);
+	public static void setLoggedInUser(HttpServletRequest request, JSONObject userJSON) 
+	{
+		request.getSession().setAttribute(USER, userJSON);
+	}
+	
+	public static Boolean validateUser(HttpServletRequest request, int UserId)
+	{
+		JSONObject loggedUser = getLoggedInUser(request);
+		return loggedUser != null && loggedUser.getInt("ID") == UserId;
 	}
 }
