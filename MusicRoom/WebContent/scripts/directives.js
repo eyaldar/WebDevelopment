356c48209116c329c1b297c5c04b6cf35ef8b793
eyaldar <eyaldar@gmail.com>
2015-02-22 20:25:55 +0200

    Removed the transit directive.
    Removed redundant room list files.
    Made the search and the navigation bar fixed.

--- MusicRoom/WebContent/scripts/directives.js
+++ MusicRoom/WebContent/scripts/directives.js
@@ -4,23 +4,6 @@
 	var musicRoom = angular.module("musicRoom");
 
 	musicRoom
-			.directive('transit', function() {
-				var linker = function(scope, element, attrs) {
-					element.hover(function() {
-						$(this).transition({
-							scale : 1.05
-						});
-					}, function() {
-						$(this).transition({
-							scale : 1
-						});
-					});
-				};
-
-				return {
-					link : linker
-				}
-			})
 			.directive(
 					"ngScopeElement",
 					function() {
